4e3c4d0abd6f2a5dd9ee7f9171b9ef8996e32e5b
AnthonyFuller <anthony@hitmods.com>
2024-12-17 06:57:41 +0000

    feat!: Peacock v8.0.0-alpha.4 is now required

--- peacock plugins/StoryModeRoulette.plugin.js
+++ peacock plugins/StoryModeRoulette.plugin.js
@@ -44,7 +44,7 @@ class MersenneTwister {
 }
 
 module.exports = function ContractSearch(controller) {
-	if (PEACOCKVERSTRING !== "8.0.0-alpha.3") {
+	if (PEACOCKVERSTRING !== "8.0.0-alpha.4") {
 		return
 	}
 	let seed = controller.transferseed
